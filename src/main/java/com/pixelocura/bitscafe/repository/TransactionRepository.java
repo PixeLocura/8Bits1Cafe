c2962d89663aa9d435763ad94418bbbce684aaf1
Daniella Vargas <idaniellavargas@gmail.com>
2025-05-15 01:44:03 -0500

    feat(transactions): add endpoints for visualizing purchase history and transactions detail (US20)

--- src/main/java/com/pixelocura/bitscafe/repository/TransactionRepository.java
+++ src/main/java/com/pixelocura/bitscafe/repository/TransactionRepository.java
@@ -4,7 +4,9 @@ package com.pixelocura.bitscafe.repository;
 import com.pixelocura.bitscafe.model.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+    List<Transaction> findByUserId(UUID userId);
 }
