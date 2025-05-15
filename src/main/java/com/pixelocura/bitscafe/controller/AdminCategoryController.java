4a1590e87ebeb7a9f86b424ee1d629c5a4576b10
LuisIsaacLazaro <luislazaro96m@hotmail.com>
2025-05-15 01:18:24 -0500

    fix:properties

--- src/main/java/com/pixelocura/bitscafe/controller/AdminCategoryController.java
+++ src/main/java/com/pixelocura/bitscafe/controller/AdminCategoryController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/admin/categories")
+@RequestMapping("/categories")
 @RequiredArgsConstructor
 public class AdminCategoryController {
 
