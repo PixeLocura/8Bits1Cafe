a993b60718359f2fadb721435a3e807cde3c2fc1
u202210494 <u202210494@upc.edu.pe>
2025-06-26 00:14:50 -0500

    feat: add method to create transaction for authenticated user with game IDs

--- src/main/java/com/pixelocura/bitscafe/service/AdminTransactionService.java
+++ src/main/java/com/pixelocura/bitscafe/service/AdminTransactionService.java
@@ -9,8 +9,14 @@ import java.util.UUID;
 
 public interface AdminTransactionService {
     List<TransactionDTO> getTransactionsByUser(UUID userId);
+
     List<TransactionDetailDTO> getTransactionDetails(UUID transactionId);
+
     TransactionDTO createTransaction(TransactionDTO transactionDTO);
+
     TransactionDTO getTransactionById(UUID id);
+
     List<TransactionDTO> getAllTransactions();
+
+    TransactionDTO createTransactionForUserAndGames(UUID userId, List<UUID> gameIds);
 }
