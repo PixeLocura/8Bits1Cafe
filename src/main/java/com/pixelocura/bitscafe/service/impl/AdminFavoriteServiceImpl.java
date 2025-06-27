0ad2fd393af6f2c5b3962433a71577356ebf1269
Crysti <103683122+crysticore@users.noreply.github.com>
2025-06-27 17:34:31 -0500

    Update AdminFavoriteServiceImpl.java

--- src/main/java/com/pixelocura/bitscafe/service/impl/AdminFavoriteServiceImpl.java
+++ src/main/java/com/pixelocura/bitscafe/service/impl/AdminFavoriteServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
