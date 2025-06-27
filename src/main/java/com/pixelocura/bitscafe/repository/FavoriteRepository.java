dea45cebba14f2e78d6e420a25f4a7abea404fb1
Crysti <103683122+crysticore@users.noreply.github.com>
2025-06-27 17:17:16 -0500

    Update FavoriteRepository.java

--- src/main/java/com/pixelocura/bitscafe/repository/FavoriteRepository.java
+++ src/main/java/com/pixelocura/bitscafe/repository/FavoriteRepository.java
@@ -3,12 +3,13 @@ package com.pixelocura.bitscafe.repository;
 import com.pixelocura.bitscafe.model.entity.Favorite;
 import com.pixelocura.bitscafe.model.entity.Favorite.FavoriteId;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.EntityGraph;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> {
+public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> { //N+1 SELECT
+    @EntityGraph(attributePaths = {"game"})
     List<Favorite> findByUser_Id(UUID userId);
     Optional<Favorite> findByUser_IdAndGame_Id(UUID userId, UUID gameId);
     void deleteByUser_IdAndGame_Id(UUID userId, UUID gameId);
