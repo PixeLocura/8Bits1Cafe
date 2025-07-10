afbcf5e55e6fa2a5230fb1bb6b422b9d7862bac1
Crysti <103683122+crysticore@users.noreply.github.com>
2025-07-10 16:45:11 -0500

    fix: allow patch cors

--- src/main/java/com/pixelocura/bitscafe/config/WebSecurityConfig.java
+++ src/main/java/com/pixelocura/bitscafe/config/WebSecurityConfig.java
@@ -128,7 +128,7 @@ public class WebSecurityConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*");
             }
         };
